d4ce0307f64b5d0e268bedfcffdb894b9862026e
Alexander Fufachev <alexfufachev@mail.ru>
2020-10-24 11:27:13 +0300

    feat: done brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let stack = []
+  let mask = []
+
+  for (let stepArray of bracketsConfig) {
+    for (let item of stepArray) {
+      mask.push(item)
+    }
+  }
+
+  for(let bracket of str) {
+
+    let bracketsIndex = mask.indexOf(bracket)
+    let bracketsLastIndex = mask.lastIndexOf(bracket)
+    let counter = 0;
+
+    if (bracketsIndex === -1){
+      continue
+    }
+      
+    if (bracketsIndex === bracketsLastIndex) {
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false
+        }
+      }
+    } 
+    else {
+      if (stack[stack.length - 1] === bracket) {
+        stack.pop()
+      } else stack.push(bracket)
+    }
+
+
+
+      
+  }
+    
+  return stack.length === 0
 }
